0111b3b9df4eb7b795a17c0071b74812563601dc
MrChozo <MrChozo@gmail.com>
2016-06-24 16:14:29 -0700

    Turtle speed

--- calculator.js
+++ calculator.js
@@ -54,46 +54,66 @@
 // JQuery for effects
 $(document).ready(function() 
 {
-	$(".key").hover(
+	// Change key color on hover
+	$(".number, .operator").hover(
 		function()
 		{
-			$(this).fadeTo("fast", 0.5);
+			$(this).addClass("active");
 		},
 		function()
 		{
-			$(this).fadeTo("fast", 1);
+			$(this).removeClass("active");
 		}
 	);
+
+	// Display button value in highScreen div
+	$(".number").click(function()
+	{
+		var numberText = $(this).text();
+		$("#highScreen").text($("#highScreen").text() + numberText);
+	});
+
+	// When operator button is pressed, display it in lowScreen
+	$("#numPlus, #numSub, #numMult, #numDiv").click(function()
+	{
+		var opText = $(this).text();
+		$("#lowScreen").text(opText);
+	});
+
+	// When Clear button is clicked, clears both screens
+	$("#numClr").click(function()
+	{
+		$("#highScreen, #lowScreen").text("");
+	});
 });
 
 var add, multiply, divide, subtract, display;
+
+var operand1 = [];
+var operand2 = [];
 
 // Takes two numbers, adds them
 function add(num1, num2)
 {
 	return num1 + num2;
 }
-
 // Takes two numbers, multiplies them
 function multiply(num1, num2)
 {
 	return num1 * num2;
 }
-
 // Takes two numbers, divides first by second
 function divide(num1, num2)
 {
 	return num1 / num2;
 }
-
 // Takes two numbers, subtracts first from second
 function subtract(num1, num2)
 {
 	return num1 - num2;
 }
 
-// Display a number/symbol in the screen div
-function display(entry)
-{
+// Clears out number arrays
 
-}
+// Enters number into array
+
