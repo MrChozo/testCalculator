74e57d173b59ca79d1c3c3385fc0814be40e4b46
MrChozo <MrChozo@gmail.com>
2016-06-24 11:47:39 -0700

    Added some HTML/CSS

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@
 var my_max,	vowel_count, reverse;
 
 // Finds maximum number out of an array.
-function my_max (numbers)
+function my_max(numbers)
 {
 	return Math.max.apply(Math, numbers);
 }
@@ -13,9 +13,8 @@ var answer = my_max(question);
 console.log(answer);*/
 
 
-
 // Takes a string and returns the number of vowels in it. (AEIOUY)
-function vowel_count (phrase)
+function vowel_count(phrase)
 {
 	var numVowels = 0;
 	var vowels = ["a", "e", "i", "o", "u", "y"];
@@ -37,9 +36,43 @@ function vowel_count (phrase)
 console.log(vowel_count(phrase));*/
 
 
-
 // Takes a string, returns reversed character order
-function reverse (saying)
+function reverse(saying)
 {
-	
+	var gniyas = [];
+	var leng = saying.length;
+	for (var i = 0; i <= saying.length; i++)
+	{
+		gniyas.push(saying[leng]);
+		leng -= 1;
+	}
+	return gniyas;
+}
+/*var saying = "Hi, how are you?";
+console.log(reverse(saying));*/
+
+
+
+// Takes two numbers, adds them
+function add(num1, num2)
+{
+	return num1 + num2;
+}
+
+// Takes two numbers, multiplies them
+function multiply(num1, num2)
+{
+	return num1 * num2;
+}
+
+// Takes two numbers, divides first by second
+function divide(num1, num2)
+{
+	return num1 / num2;
+}
+
+// Takes two numbers, subtracts first from second
+function subtract(num1, num2)
+{
+	return num1 - num2;
 }
